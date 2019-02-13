c3a72d10a10e8071dbbb6e02be859d1a62f93f27
Indah Widya Agustin <indahwidyaagustin22@gmail.com>
2019-02-13 08:46:01 +0700

    eps-5

--- resources/views/auth/.login.blade.php
+++ resources/views/auth/.login.blade.php
@@ -1,17 +1,16 @@
 @extends('auth.main')
 @section('title','login')
 @section('content')
-<div class="card card-login mx-auto mt-5">
+ <div class="card card-login mx-auto mt-5">
         <div class="card-header">Login</div>
         <div class="card-body">
           <form method="POST" action="{{route('login')}}">
             {{csrf_field()}}
             <div class="form-group">
               <div class="form-label-group">
-                <input type="email" id="inputEmail" 
-                class="form-control{{$errors->has('email') ? 'is-invalid' : ''}}"
+                <input type="email" id="inputEmail" class="form-control{{$errors->has('email') ? 'is-invalid' : ''}}" 
                 name="email"
-                 placeholder="Email address" required="required" autofocus="autofocus">
+                placeholder="Email address" required="required" autofocus="autofocus">
                 <label for="inputEmail">Email address</label>
                 @if($errors->has('email'))
                 <div class="invalid-feedback">{{$errors->first('email')}}</div>
@@ -20,24 +19,24 @@
             </div>
             <div class="form-group">
               <div class="form-label-group">
-                <input type="password" id="inputPassword" class="form-control{{$errors->has('password') ? 'is-invalid' : '' }}"
+                <input type="password" id="inputPassword" class="form-control {{$errors->has('password') ? 'is-invalid' : '' }}"
                 name="password"
                  placeholder="Password" required="required">
                 <label for="inputPassword">Password</label>
                 @if($errors->has('password'))
-                <div class="invalid-feedback">{{$errors->first('password')}}</div>
+                <div class ="invalid-feedback" >{{$errors->first('password')}}</div>
                 @endif
               </div>
             </div>
             <div class="form-group">
               <div class="checkbox">
                 <label>
-                  <input type="checkbox" name="remember" {{ old('remember') ? 'checked':'' }}>
+                  <input type="checkbox" name="remember" {{ old('remember') ? 'checked': ''}}"">
                   Remember Password
                 </label>
               </div>
             </div>
-            <button class="btn btn-primary btn-block type="submit">Login</button>
+            <button class="btn btn-primary btn-block" type="submit">Login</a>
           </form>
           <div class="text-center">
             <a class="d-block small" href="{{route('password.request')}}">Forgot Password?</a>
@@ -45,4 +44,3 @@
         </div>
  </div>
 @endsection
-
